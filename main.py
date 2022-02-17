52086916103f711f08b812d901ccebe6f2946460
Athar Shaikh <atharshaikh53@gmail.com>
2022-02-17 15:56:15 +0530

    Round Robin Algorithm

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
 import sys
 
 import fcfs
+import rr
 import sjf_np
 import sjf_p
 from genfunc import generate_tasks, get_turnaround_time, get_waiting_time, show_table
@@ -8,10 +9,11 @@ from genfunc import generate_tasks, get_turnaround_time, get_waiting_time, show_
 if __name__ == "__main__":
     choice = int(
         input(
-            "Enter 1 for FCFS implementation, 2 for SJF-NP implementation, and 3 for SJF-P implementation\n"
+            "Enter 1 for FCFS implementation, 2 for SJF-NP implementation, 3 for SJF-P"
+            " implementation, and 4 for RR implementation.\n"
         )
     )
-    if choice not in [1, 2, 3]:
+    if choice not in [1, 2, 3, 4]:
         print("Invalid choice!!")
         sys.exit(-1)
     tasks = generate_tasks()
@@ -25,6 +27,11 @@ if __name__ == "__main__":
         impl = "SJF-P"
         sjf_p.get_completion_time(tasks)
         sjf_p.get_response_time(tasks)
+    elif choice == 4:
+        tq = int(input("Enter the time quantum\n"))
+        impl = "RR"
+        rr.get_completion_time(tasks, tq)
+        rr.get_response_time(tasks)
 
     get_turnaround_time(tasks)
     get_waiting_time(tasks)
--- rr.py
+++ rr.py
@@ -3,26 +3,27 @@ from typing import List
 from task import Task
 
 
-def get_completion_time(task_list: List[Task]) -> None:
+def get_completion_time(task_list: List[Task], time_quantum: int):
     current_time = 0
     task_queue = task_list[:]
     remaining_tasks = len(task_queue)
     ready_queue = []
+    current_task = None
     while remaining_tasks > 0:
         while task_queue and task_queue[0].arrival_time <= current_time:
             ready_queue.append(task_queue.pop(0))
-        ready_queue.sort(key=lambda x: (x.time_left, x.arrival_time))
+        if current_task and current_task.time_left != 0:
+            ready_queue.append(current_task)
         if len(ready_queue) == 0:
             current_time += 1
             continue
         current_task = ready_queue.pop(0)
         if current_task.time_left == current_task.burst_time:
             current_task.first_cpu_time = current_time
-        current_time += 1
-        current_task.time_left -= 1
-        if current_task.time_left > 0:
-            ready_queue.append(current_task)
-        elif current_task.time_left == 0:
+        work_time = min(current_task.time_left, time_quantum)
+        current_time += work_time
+        current_task.time_left -= work_time
+        if current_task.time_left == 0:
             current_task.completion_time = current_time
             remaining_tasks -= 1
 
